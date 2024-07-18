816cef056c5f87161704140de6167a18f65ff0dd
Alice Chan <alicebay10@yahoo.co.uk>
2024-07-18 12:54:22 +0000

    Made examples for Vanilla Javascript Funciton & Arrow Function with Parameters.

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,10 +4,27 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3 ,5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+ const addTwoNumbers = (a, b) => {
+     // Code block
+     return a + b;
+ }
+ let sum = addTwoNumbers(3 ,5);
+ console.log(sum);
+
 
 // Single Line Arrow Function With Parameters
+//const addTwoNumbers = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 
 // Implicit Returns
 
